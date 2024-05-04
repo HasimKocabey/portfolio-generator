a89e6edcea9ca9ba16d1fb53b0e51679bf254dbe
HasimKocabey <hasimmk92@gmail.com>
2024-05-04 14:49:54 +0200

    test erfolgreich

--- script.test.js
+++ script.test.js
@@ -15,6 +15,16 @@ const mockPreviewButtonElement = document.createElement('button');
 mockPreviewButtonElement.id = 'preview-button';
 document.body.appendChild(mockPreviewButtonElement);
 
+const mockDownloadButtonElement = document.createElement('button');
+mockDownloadButtonElement.id = 'download-button';
+document.body.appendChild(mockDownloadButtonElement);
+
+// Mocke das 'info-form'-Element
+const mockInfoFormElement = document.createElement('form');
+mockInfoFormElement.id = 'info-form';
+document.body.appendChild(mockInfoFormElement);
+
+
 // Schreibe einen Testfall für handleInput-Funktion
 test('Teste handleInput Funktion', () => {
   // Simuliere ein Ereignisobjekt
@@ -38,5 +48,14 @@ test('Teste Event-Listener in handleInput Funktion', () => {
   // Überprüfe, ob der Klick auf den Vorschau-Button das erwartete Verhalten auslöst
   document.getElementById('preview-button').click();
   expect(document.getElementById('preview').classList).not.toContain('hidden'); // Überprüfe, ob die Vorschau sichtbar wird
-  // Führe weitere Überprüfungen für andere Event-Listener durch
+ 
+  // Überprüfe, ob der Klick auf den Download-Button das erwartete Verhalten auslöst
+  document.getElementById('download-button').click();
+  // Füge hier weitere Überprüfungen für das erwartete Verhalten beim Klick auf den Download-Button hinzu, z.B. Überprüfung des Downloads
+
+  // Überprüfe, ob der Input-Event auf dem Formular das erwartete Verhalten auslöst
+  const inputForm = document.getElementById('info-form');
+  inputForm.dispatchEvent(new Event('input'));
+  // Füge hier weitere Überprüfungen für das erwartete Verhalten beim Input-Event hinzu, z.B. Überprüfung, ob die handleInput-Funktion aufgerufen wird
 });
+
